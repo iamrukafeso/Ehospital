a58d4b4310094c46fee2294849fe98fcbc4d61fe
iamrukafeso <iamrukafeso@gmail.com>
2020-04-13 17:11:40 +0100

    Covid-19 Tracer implementation- need to fix error

---
+++ app/src/main/java/com/ehospital/covid19.java
@@ -0,0 +1,29 @@
+package com.ehospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class covid19 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covid19);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        navView.setItemIconTintList(null);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+}
