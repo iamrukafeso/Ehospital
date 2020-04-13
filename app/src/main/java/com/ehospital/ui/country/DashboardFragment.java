7404279daa86f9e3760fb6e37b0fe81b4d2301b6
hamarow <hamarow114@gmail.com>
2020-04-13 22:15:14 +0100

    fixed the covid tracker

---
+++ app/src/main/java/com/ehospital/ui/country/DashboardFragment.java
@@ -0,0 +1,25 @@
+package com.ehospital.ui.country;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.ehospital.R;
+
+public class DashboardFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_country, container, false);
+
+        return root;
+    }
+}
